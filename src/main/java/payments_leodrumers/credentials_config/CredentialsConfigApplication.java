363e87ffbf300bb3314f79b7363cff57d7c3a724
Leandro Ortiz <leortiz@squaretrade.com>
2024-12-10 11:54:49 -0500

    initial spring commit

---
+++ src/main/java/payments_leodrumers/credentials_config/CredentialsConfigApplication.java
@@ -0,0 +1,13 @@
+package payments_leodrumers.credentials_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialsConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentialsConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/payments_leodrumers/credentials_config/CredentialsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package payments_leodrumers.credentials_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
